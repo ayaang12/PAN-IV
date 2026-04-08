0f1db038bb68cbd8873c4487d585aee5ff997520
neelyp <neel.pandruvada@mcvts.org>
2026-04-08 18:52:22 -0400

    get rid of unused packages

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,13 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import {
-  LayoutDashboard,
-  Activity,
-  Bell,
-  Users,
-  Settings,
-  Heart,
-  Shield,
-} from "lucide-react";
+import { LayoutDashboard, Activity, Bell, Users, Shield } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 const navItems = [
